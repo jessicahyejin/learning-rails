7864bc5698740539d12ec0d39579e5ee880905ac
vagrant <vagrant@learning-rails>
2013-08-24 19:40:03 +0000

    add users and posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Microposts::Application.config.secret_key_base = 'a84973203d033606ab79134d869d679cce7ddb67a9da577d5c4b0b186bd97c62aafe2f2c92500a0244b951883f4bd172e49c59d7efe413a8508eb53d012f9e2f'
